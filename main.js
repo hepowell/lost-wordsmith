8da15c73b3323fd53180014a486ebe3d286c01f4
Hannah Powell <hannah.powell.00@gmail.com>
2023-12-22 11:05:01 -0700

    first puzzle question added

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const gameContainer = document.getElementById('game-container');
 
     // Initial game setup
-    output.innerHTML = 'In a world where words have power, a legendary wordsmith has disappeared, leaving behind a series of cryptic clues that only a true wordsmith can decipher.';
+    const welcomeMessage = 'In a world where words have power, a legendary wordsmith has disappeared, leaving behind a series of cryptic clues that only a true wordsmith can decipher... Are you up to the challenge?'
+    typeWriter(welcomeMessage);
+
 
     // Event listener for user input
     input.addEventListener('keyup', function (event) {
@@ -16,6 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Function to handle user input and update game output
     function handleInput(inputText) {
+
         // Process the input and generate the appropriate response
         const response = processInput(inputText);
 
@@ -29,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Function to simulate typewriter effect
     function typeWriter(text) {
         let index = 0;
-        const speed = 50; // Adjust the speed as needed (lower value for faster typing)
+        const speed = 40; //(lower value for faster typing)
 
         function type() {
             if (index < text.length) {
@@ -42,16 +45,29 @@ document.addEventListener('DOMContentLoaded', function () {
         type();
     }
 
+
     // Function to process user input and generate a response
     function processInput(inputText) {
-        // Add your word puzzle logic here
-        // You can check inputText against expected answers and return the corresponding response
+        // Check inputText against expected answers and return the corresponding response
 
-        // For example:
-        if (inputText.toLowerCase() === 'hello') {
-            return 'Hi there!';
-        } else {
+        if (inputText.toLowerCase() === 'yes' || inputText.toLowerCase() === "y") {
+            return puzzleOne()
+        }
+        else if (inputText.toLowerCase() === "hello" || inputText.toLowerCase() === "hi") {
+            return "Do you care to answer my question? Are you ready?"
+        }
+        else if (inputText.toLowerCase() === "okay" || inputText.toLowerCase() === "ok") {
+            return puzzleOne()
+        }
+        else if (inputText.toLowerCase() === "no" || inputText.toLowerCase() === "n") {
+            return "What a shame, maybe some other time"
+        }
+         else {
             return "I didn't understand that.";
         }
+    }
+
+    function puzzleOne() {
+        return "The Enchanted Garden: A mystical creature whispers, 'In this room, a secret does loom.' The flowers respond, revealing the secret of growth. Find the word that makes the flowers respond and proceed."
     }
 });
